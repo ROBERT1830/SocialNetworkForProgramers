c1153ce6685ef66d4b763d9252a30dfac25c9849
ROBERT1830 <robert@beatingbrain.com>
2022-04-10 19:55:38 +0200

    Basic structure

---
+++ app/src/main/java/com/robertconstantindinescu/my_social_network/presentation/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.robertconstantindinescu.my_social_network.presentation
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.ui.Modifier
+import com.robertconstantindinescu.my_social_network.presentation.ui.theme.My_social_networkTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            My_social_networkTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    color = MaterialTheme.colors.background,
+                    modifier = Modifier.fillMaxSize()
+
+                ){
+
+                }
+            }
+        }
+    }
+}
+
---
+++ app/src/main/java/com/robertconstantindinescu/my_social_network/presentation/ui/theme/Theme.kt
@@ -0,0 +1,46 @@
+package com.robertconstantindinescu.my_social_network.presentation.ui.theme
+
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
+import androidx.compose.runtime.Composable
+
+private val DarkColorPalette = darkColors(
+    primary = GreenAccent,
+    primaryVariant = Purple700,
+    background = DarkGray,
+    //background for some things like cards
+    onBackground = TextWhite,
+    onPrimary = DarkGray,
+    surface = MediumGray,
+    onSurface = LightGray
+
+
+)
+
+private val LightColorPalette = lightColors(
+    primary = Purple500,
+    primaryVariant = Purple700,
+    secondary = Teal200
+
+    /* Other default colors to override
+    background = Color.White,
+    surface = Color.White,
+    onPrimary = Color.White,
+    onSecondary = Color.Black,
+    onBackground = Color.Black,
+    onSurface = Color.Black,
+    */
+)
+
+@Composable
+fun My_social_networkTheme(
+    content: @Composable () -> Unit
+) {
+    MaterialTheme(
+        colors = DarkColorPalette,
+        typography = Typography,
+        shapes = Shapes,
+        content = content
+    )
+}
